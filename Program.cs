494fe8447c5cca5c06306627c531e3bda88567da
Daniil Drahin <dragindaniilmeizu@gmail.com>
2024-07-07 22:08:21 +0300

    add cors

--- Program.cs
+++ Program.cs
@@ -13,6 +13,8 @@ builder.Services.AddDbContext<DataContext>(opt =>
 
 var app = builder.Build();
 
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
 app.MapControllers();
 
 app.Run();
